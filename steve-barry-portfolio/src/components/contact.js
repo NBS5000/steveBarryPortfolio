c3e83e58f27eb6b1397b07c89d135b930d21bffc
NBS5000 <steve.nbs.barry@gmail.com>
2022-05-05 12:48:01 +1000

    css updated for existing and new components, contact and footer details updated, including acknowledgement

--- steve-barry-portfolio/src/components/contact.js
+++ steve-barry-portfolio/src/components/contact.js
@@ -4,13 +4,13 @@ function Contact() {
     return (  
         <div id="contact">
             <div className="contactIcon">
-                <link href="mailto:nbs5000.dev@gmail.com"><img className="contactImg" src="./assets/images/email.png" alt="Email Icon" /></link>
+                <a href="mailto:nbs5000.dev@gmail.com"><img className="contactImg" src="./assets/images/email.png" alt="Email Icon" /></a>
             </div>
             <div className="contactIcon">
-                <link href="https://github.com/NBS5000" target="_blank" rel="noopener noreferrer"><img className="contactImg" src="./assets/images/github.png" alt="Github Icon" /></link>
+                <a href="https://github.com/NBS5000" target="_blank" rel="noopener noreferrer"><img className="contactImg" src="./assets/images/github.png" alt="Github Icon" /></a>
             </div>
             <div className="contactIcon">
-                <link href="https://www.linkedin.com/in/stephen-barry-676306108/" target="_blank" rel="noopener noreferrer"><img className="contactImg" src="./assets/images/linkedin.png" alt="LinkedIn Icon" /></link>
+                <a href="https://www.aedin.com/in/stephen-barry-676306108/" target="_blank" rel="noopener noreferrer"><img className="contactImg" src="./assets/images/linkedin.png" alt="aedIn Icon" /></a>
             </div>
             <div className="contactIcon" id="burger2">
                 <img className="contactImg" src="./assets/images/burger.png" alt="Burger Icon" />
--- steve-barry-portfolio/src/components/footer.js
+++ steve-barry-portfolio/src/components/footer.js
@@ -6,9 +6,9 @@ function Footer() {
             <div id="ball4"></div>
             <div id="ftBody"></div>
             <h5 id="copy">&copy; Steve Barry 2022</h5>
-            <link href="https://www.darkinjung.com.au/" target="_blank" rel="noopener noreferrer"><img id="ackImg" src="./assets/images/flag.png" alt="Aboriginal Flag" /></link>
+            <a href="https://www.darkinjung.com.au/" target="_blank" rel="noopener noreferrer"><img id="ackImg" src="./assets/images/flag.png" alt="Aboriginal Flag" /></a>
             <p id="ack">I today acknowledge the Darkinjung people Traditional Custodians of the land on which I live today, and pay my respects to their Elders past and present. 
-            <br/>I extend that respect to all Aboriginal and Torres Strait Islander peoples.</p>
+            I extend that respect to all Aboriginal and Torres Strait Islander peoples.</p>
         </footer>
     )
 }
