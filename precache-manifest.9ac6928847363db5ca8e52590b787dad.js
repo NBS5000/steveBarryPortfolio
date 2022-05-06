976fe1e599248541ed27ffc9d228c4a7563eb45c
NBS5000 <steve.nbs.barry@gmail.com>
2022-05-07 00:21:17 +1000

    Updates

---
+++ precache-manifest.9ac6928847363db5ca8e52590b787dad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "23dfa3fd0cc0b828f812",
+    "url": "/steveBarryPortfolio/static/css/main.c46be760.chunk.css"
+  },
+  {
+    "revision": "23dfa3fd0cc0b828f812",
+    "url": "/steveBarryPortfolio/static/js/main.23dfa3fd.chunk.js"
+  },
+  {
+    "revision": "8c98aab82769540efb62",
+    "url": "/steveBarryPortfolio/static/js/1.8c98aab8.chunk.js"
+  },
+  {
+    "revision": "fe9ca1ba5eb3be9ae27f",
+    "url": "/steveBarryPortfolio/static/js/2.fe9ca1ba.chunk.js"
+  },
+  {
+    "revision": "7701d945825bfd494bb5",
+    "url": "/steveBarryPortfolio/static/js/runtime~main.7701d945.js"
+  },
+  {
+    "revision": "4d454067029c054510527731d36c7863",
+    "url": "/steveBarryPortfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5e9a54f41a72f3335a71bf044eeaf91a.js"
+  "/steveBarryPortfolio/precache-manifest.9ac6928847363db5ca8e52590b787dad.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/steveBarryPortfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
