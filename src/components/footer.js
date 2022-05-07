47eda310430f1d01d121a1aaeb2d71218c79b2e1
NBS5000 <steve.nbs.barry@gmail.com>
2022-05-07 23:02:05 +1000

    ack moved to personal, personal adjusted

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,9 +9,6 @@ function Footer() {
             </div>
             <div id="ftBody"></div>
             <h5 id="copy">&copy; Steve Barry 2022</h5>
-            <p id="ack"><a href="https://www.darkinjung.com.au/" target="_blank" rel="noopener noreferrer"><img id="ackImg" src="./assets/images/flag.png" alt="Aboriginal Flag" /></a>
-            I today acknowledge the Darkinjung people Traditional Custodians of the land on which I live today, and pay my respects to their Elders past and present. 
-            I extend that respect to all Aboriginal and Torres Strait Islander peoples.</p>
         </footer>
     )
 }
--- src/components/personal.js
+++ src/components/personal.js
@@ -4,8 +4,10 @@ function Personal() {
     return (  
         <div id="personal">
             <h3 className="marker">Personal</h3>
-            <p className='aboutP'>An avid Star Wars fan, I enjoy gaming, Lego, collecting wine (the centrepiece of my collection being a 2008 Penfolds Grange), 
-                singing terribly in the car, and playing any games my three year old has thought up!</p>
+            <p className='aboutP'>Hello there! Here's a little about me... I'm an avid Star Wars fan, I enjoy gaming, Lego, collecting wine (the centrepiece of my collection being a 2008 Penfold's Grange, and I also have wines from the 70s, 80s, 90s, as well as the 21st century), 
+                singing terribly in the car to an eclectic range of music, and playing any games my three year old has thought up!</p>
+            <p className='aboutP'>I today acknowledge the <a id="ackLink" href="https://www.darkinjung.com.au/" target="_blank" rel="noopener noreferrer">Darkinjung</a> people Traditional Custodians of the land on which I live today, and pay my respects to their Elders past and present. 
+            I extend that respect to all Aboriginal and Torres Strait Islander peoples.</p>
         </div>
     )
 }
