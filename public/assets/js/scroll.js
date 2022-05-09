4ae14fa49bee60b7b89c7ae5ec149897ce2baf36
NBS5000 <steve.nbs.barry@gmail.com>
2022-05-09 18:46:57 +1000

    fixed vert/horz scroll issue, tidied up testing code

--- public/assets/js/scroll.js
+++ public/assets/js/scroll.js
@@ -1,23 +1,16 @@
 /* scroll script */
 const el = document.getElementById("displayList");
 el.addEventListener("wheel", function (event) {
-    const div = document.getElementById("code");
+    event.preventDefault();
     let x = el.scrollLeft;
     let y = el.scrollTop;
-    let b_y = div.scrollTop;
 
     if (event.deltaY > 0) {
+        
         x += 80;
     } else {
         x -= 80;
     }
     el.scrollTo(x,y);
-        event.deltaX = 0;
-    // window.scrollTo(0, b_y);
-    console.log(b_y);
-    });
 
-    el.addEventListener("hover", function (event) {
-
-    window.scrollTo(0, y);
 })
