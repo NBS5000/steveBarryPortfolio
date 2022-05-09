633058f0209d0b891c885c269bc992b3378bfc35
NBS5000 <steve.nbs.barry@gmail.com>
2022-05-09 18:05:08 +1000

    readded scripts to index

--- src/components/menuModal.js
+++ src/components/menuModal.js
@@ -16,7 +16,7 @@ function MenuModal() {
                         </div>
                         <div className="modalContact">
                             <div className="contactIcon">
-                                <a className="modContactLink" href="https://github.com/NBS5000" target="_blank">
+                                <a className="modContactLink" href="https://github.com/NBS5000" target="_blank"  rel="noopener noreferrer">
                                     <img className="contactImg" src="./assets/images/github.png" alt="Github Icon" />
                                     <p className="contactP">https://github.com/NBS5000</p>
                                 </a>
@@ -24,7 +24,7 @@ function MenuModal() {
                         </div>
                         <div className="modalContact">
                             <div className="contactIcon">
-                                <a className="modContactLink" href="https://www.linkedin.com/in/stephen-barry-676306108/" target="_blank">
+                                <a className="modContactLink" href="https://www.linkedin.com/in/stephen-barry-676306108/" target="_blank"  rel="noopener noreferrer">
                                     <img className="contactImg" src="./assets/images/linkedin.png" alt="LinkedIn Icon" />
                                     <p className="contactP">https://www.linkedin.com/in/stephen-barry-676306108/</p>
                                 </a>
@@ -32,7 +32,7 @@ function MenuModal() {
                         </div>
                         <div className="modalContact">
                             <div className="contactIcon">
-                                <a className="modContactLink" href="https://www.google.com/maps/place/Terrigal+NSW+2260/@-33.4457731,151.4113257,14z/data=!3m1!4b1!4m5!3m4!1s0x6b72b58677fc5889:0x5017d681632de80!8m2!3d-33.448011!4d151.444461" target="_blank">
+                                <a className="modContactLink" href="https://www.google.com/maps/place/Terrigal+NSW+2260/@-33.4457731,151.4113257,14z/data=!3m1!4b1!4m5!3m4!1s0x6b72b58677fc5889:0x5017d681632de80!8m2!3d-33.448011!4d151.444461" target="_blank"  rel="noopener noreferrer">
                                     <img className="contactImg" src="./assets/images/map.png" alt="Google Maps Icon" />
                                     <p className="contactP">Terrigal, NSW</p>
                                 </a>
@@ -40,7 +40,7 @@ function MenuModal() {
                         </div>
                         <div className="modalContact">
                             <div id="optionsCV" className="contactIcon">
-                                <a id="viewCV" className="modContactLink" href="https://docs.google.com/document/d/1rUfsYP4VY3RjZRth_pvh2s8ZYj7r6FfjqfcYySy1COc/edit?usp=sharing" target="_blank">
+                                <a id="viewCV" className="modContactLink" href="https://docs.google.com/document/d/1rUfsYP4VY3RjZRth_pvh2s8ZYj7r6FfjqfcYySy1COc/edit?usp=sharing" target="_blank"  rel="noopener noreferrer">
                                     <img className="contactImg" src="./assets/images/resume.png" alt="Resume Icon" />
                                     <p id="viewCvP" className="contactP">Steve Barry - Resume</p>
                                 </a>
