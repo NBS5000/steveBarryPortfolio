daaa72dbe45ff361109db7e3721de439973c0d6b
NBS5000 <steve.nbs.barry@gmail.com>
2022-05-09 15:59:12 +1000

    change scroll script location

--- public/assets/js/scroll.js
+++ public/assets/js/scroll.js
@@ -1,7 +1,23 @@
-
-
-var el = document.getElementById("displayList");
+/* scroll script */
+const el = document.getElementById("displayList");
 el.addEventListener("wheel", function (event) {
-    if (event.deltaY > 0) el.scrollLeft += 80;
-    else el.scrollLeft -= 80;
-});
+    const div = document.getElementById("code");
+    let x = el.scrollLeft;
+    let y = el.scrollTop;
+    let b_y = div.scrollTop;
+
+    if (event.deltaY > 0) {
+        x += 80;
+    } else {
+        x -= 80;
+    }
+    el.scrollTo(x,y);
+        event.deltaX = 0;
+    // window.scrollTo(0, b_y);
+    console.log(b_y);
+    });
+
+    el.addEventListener("hover", function (event) {
+
+    window.scrollTo(0, y);
+})
