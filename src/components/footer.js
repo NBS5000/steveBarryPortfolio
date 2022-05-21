d1d1eb76d3a82c330b305be5377f04801085336f
NBS5000 <steve.nbs.barry@gmail.com>
2022-05-21 20:44:06 +1000

    finished look of page

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,12 +3,21 @@ import React from 'react';
 function Footer() {
     return (  
         <footer>
-            <div id="ball2"></div>
-            <div id="ballWrap">
-                <div id="ball4"></div>
+
+            <div>
+                <div id="ball2"></div>
+                <div id="ballWrap">
+                    <div id="ball4"></div>
+                </div>
+                <h5 id="copy">&copy; Steve Barry 2022</h5>
+                
+                <div id="footerBlur">
+                    <div id="footerBlurExtra">
+                    <p id='footP'>I today acknowledge the <a id="ackLink" href="https://www.darkinjung.com.au/" target="_blank" rel="noopener noreferrer">Darkinjung</a> people Traditional Custodians of the land on which I live today, and pay my respects to their Elders past and present. 
+                        I extend that respect to all Aboriginal and Torres Strait Islander peoples.</p>
+                    </div>
+                </div>
             </div>
-            <div id="ftBody"></div>
-            <h5 id="copy">&copy; Steve Barry 2022</h5>
         </footer>
     )
 }
